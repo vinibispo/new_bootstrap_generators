138271aa28e6f7eb96d90764ba5e603a644dfb3d
vinibispo <vini.bispo015@gmail.com>
2021-09-25 18:46:19 -0300

    feat: add first setup on webpacker on rails engine

--- lib/new_bootstrap_generators.rb
+++ lib/new_bootstrap_generators.rb
@@ -2,5 +2,10 @@ require "new_bootstrap_generators/version"
 require "new_bootstrap_generators/engine"
 
 module NewBootstrapGenerators
+  class << self
+    def Webpacker
+      @Webpacker ||= ::Webpacker::Instance.new(root_path: NewBootstrapGenerators::Engine.root, config_path: NewBootstrapGenerators::Engine.root.join('config', 'webpacker.yml'))
+    end
+  end
   # Your code goes here...
 end
