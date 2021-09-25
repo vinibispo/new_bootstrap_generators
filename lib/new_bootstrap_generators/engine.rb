f99be531b8dae6c0ebf2af74b54b44026a32ada0
vinibispo <vini.bispo015@gmail.com>
2021-09-25 19:30:36 -0300

    feat: improve readability on engine

--- lib/new_bootstrap_generators/engine.rb
+++ lib/new_bootstrap_generators/engine.rb
@@ -3,17 +3,7 @@
 module NewBootstrapGenerators
   class Engine < ::Rails::Engine
     isolate_namespace NewBootstrapGenerators
-    initializer 'webpacker.proxy' do |app|
-      insert_middleware = NewBootstrapGenerators.webpacker.config.dev_server.present? rescue nil
-      next unless insert_middleware
-
-      app.middleware.insert_before(
-        0, Webpacker::DevServerProxy,
-        ssl_verify_none: true,
-        webpacker: NewBootstrapGenerators.webpacker
-      )
-
-      app.middleware.insert_before(0, Rack::Static, urls: ['/new_bootstrap_generators-packs'], root: NewBootstrapGenerators::Engine.root.join('public').to_s)
-    end
+    config.app_middleware.insert_before(0, Rack::Static, urls: ['/new_bootstrap_generators-packs'],
+                                                         root: NewBootstrapGenerators::Engine.root.join('public').to_s)
   end
 end
