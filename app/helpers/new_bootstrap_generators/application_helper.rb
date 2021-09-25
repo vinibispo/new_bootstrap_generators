ef1be1d25a759b411ae0e9b01f71e8e3980f4c37
vinibispo <vini.bispo015@gmail.com>
2021-09-25 19:21:05 -0300

    feat: include Webpacker on helper

--- app/helpers/new_bootstrap_generators/application_helper.rb
+++ app/helpers/new_bootstrap_generators/application_helper.rb
@@ -1,4 +1,11 @@
+require 'webpacker/helper'
+
 module NewBootstrapGenerators
   module ApplicationHelper
+    include ::Webpacker::Helper
+
+    def current_webpacker_instance
+      MyEngine.webpacker
+    end
   end
 end
