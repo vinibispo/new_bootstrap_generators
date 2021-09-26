cb41f776c0a139a7f762d286a610c0369f629890
vinibispo <vini.bispo015@gmail.com>
2021-09-25 21:49:22 -0300

    fix: fix generator module

--- lib/generators/new_bootstrap_generators/install/install_generator.rb
+++ lib/generators/new_bootstrap_generators/install/install_generator.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
+require 'rails/generators'
+
 module NewBootstrapGenerators
-  module NewBootstrapGenerators
+  module Generators
     class InstallGenerator < ::Rails::Generators::Base
       source_root File.expand_path('templates', __dir__)
 
       desc 'Full Installation of new_bootstrap_generators'
 
-      def copy_layout_file
-        copy_file 'layouts/starter.html.erb', 'app/views/layouts/application.html.erb'
-      end
+      # def copy_layout_file
+      #  copy_file 'layouts/starter.html.erb', 'app/views/layouts/application.html.erb'
+      # end
 
       def copy_scaffold_files
-        copy_dir 'erb/scaffold', 'lib/templates/erb/scaffold'
+        empty_directory File.join('lib', 'templates')
+        empty_directory File.join('lib/templates/erb')
+        empty_directory File.join('lib/templates/erb/scaffold')
+
+        available_views.each do |view|
+          copy_file "erb/scaffold/#{view}.html.erb", File.join('lib/templates/erb/scaffold', "#{view}.html.erb")
+        end
+      end
+
+      private
+
+      def available_views
+        %w[index edit show new _form]
       end
     end
   end
