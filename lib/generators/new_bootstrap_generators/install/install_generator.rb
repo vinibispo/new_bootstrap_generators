ba4d27a1a16d06b573ae787dabe37753e8a89bd9
vinibispo <vini.bispo015@gmail.com>
2021-09-26 16:25:06 -0300

    feat: add install_dependencies on generator install

--- lib/generators/new_bootstrap_generators/install/install_generator.rb
+++ lib/generators/new_bootstrap_generators/install/install_generator.rb
@@ -23,10 +23,52 @@ module NewBootstrapGenerators
         end
       end
 
+      def add_yarn_dependencies
+        puts destination_root
+        unless File.exist?('package.json')
+          say 'Verify if webpacker is installed', :green
+          install_webpacker if gemfile_exists?
+        end
+        install_bootstrap
+      end
+
       private
 
       def available_views
         %w[index edit show new _form]
+      end
+
+      def gemfile_exists?
+        File.exist?('Gemfile')
+      end
+
+      def gemfile_has_webpacker?
+        File.read('Gemfile').include?('webpacker')
+      end
+
+      def install_webpacker
+        if gemfile_has_webpacker?
+          gemfile_lock = File.read('Gemfile.lock')
+          if !gemfile_lock.present? || !gemfile_lock.include?('webpacker')
+            say "Webpacker is not installed, so we're gonna install", :green
+            run 'bundle install'
+          end
+        else
+          add_webpacker_on_gemfile
+        end
+      end
+
+      def add_webpacker_on_gemfile
+        say 'Webpacker is not on Gemfile', :green
+        run 'bundle add webpacker'
+        run 'bundle install'
+        say 'Configuring webpacker', :green
+        run 'bundle exec rails generate webpacker:install'
+      end
+
+      def install_bootstrap
+        say 'Install bootstrap dependencies', :green
+        run 'yarn add bootstrap@^5.1.1 @popperjs/core@^2.10.1'
       end
     end
   end
