21b799136adfe83479c1d36d8ce091778bc93b78
vinibispo <vini.bispo015@gmail.com>
2021-09-25 21:50:41 -0300

    test: create our first test for generator

---
+++ test/generators/install_generator_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'generators/new_bootstrap_generators/install/install_generator'
+
+class InstallGeneratorTest < Rails::Generators::TestCase
+  tests NewBootstrapGenerators::Generators::InstallGenerator
+  destination File.expand_path('../../tmp', __dir__)
+
+  setup do
+    prepare_destination
+    prepare_dummy_app
+  end
+
+  test 'should copy scaffold erb templates' do
+    run_generator
+    %w[index edit new show _form].each do |view|
+      assert_file "lib/templates/erb/scaffold/#{view}.html.erb"
+    end
+  end
+
+  private
+
+  def prepare_dummy_app
+    FileUtils.cp_r('test/dummy/.', destination_root)
+  end
+end
